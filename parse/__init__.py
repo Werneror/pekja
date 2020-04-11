4c5d5e1c49c1cf38e6dce0237d3ee8e890837d7c
Werner <me@werner.wiki>
2020-04-11 22:28:46 +0800

    修复小bug

--- parse/__init__.py
+++ parse/__init__.py
@@ -10,7 +10,7 @@ def get_parse_class_list():
     pkg_path = os.path.dirname(__file__)
     parse_class_list = list()
     for _, file, _ in pkgutil.iter_modules([pkg_path]):
-        module = importlib.import_module('parse.src.' + file)
+        module = importlib.import_module('parse.' + file)
         parse_class_list += [m[0] for m in inspect.getmembers(module, inspect.isclass)
                              if issubclass(m[1], Parser) and m[0] != 'Parser']
     return parse_class_list
